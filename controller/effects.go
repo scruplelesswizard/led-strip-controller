56d2685f888d2a520f390b09692d9e683c8a21cd
chaosaffe <jason@chaosaffe.io>
2016-11-21 23:48:06 +0100

    Move effects out to effects.go

---
+++ controller/effects.go
@@ -0,0 +1,76 @@
+package controller
+
+import "time"
+
+func (s *Strip) Off() {
+	s.SetColor(RGB.Off())
+}
+
+func (s *Strip) Fade(color RGB, duration time.Duration) {
+
+	// calculate step duration and # of steps
+	stepDuration := time.Duration(100) * time.Millisecond
+	steps := duration / stepDuration
+	maxDiff = s.Color.maxDifference(color)
+
+	if steps > maxDiff {
+		steps = maxDiff
+		stepDuration = duration / steps
+	}
+
+	// calculate differences
+
+	// TODO: Fades should be done in the HSV space for more appeasing gradients
+
+	rDiff, gDiff, bDiff := s.Color.difference(color)
+
+	// calculate change per steps
+
+	// This will cause fractional steps to be discarded, meaning there
+	// is the possibility that changes won't occur, or that the final color
+	// won't be the requested color
+
+	rStep = rDiff / steps
+	gStep = gDiff / steps
+	bStep = bDiff / steps
+
+	for step := 0; step < count; step++ {
+		s.SetColor(s.Color.Add(rStep, gStep, bStep))
+		time.Sleep(stepDuration)
+	}
+
+	// HACK: to ensure the final color is the requested color
+
+	s.SetColor(color)
+
+}
+
+func (s *Strip) FadeBetween(a, b RGB, duration time.Duration) {
+
+	s.Fade(a, duration/2)
+	// HACK: This will block. Consider using channel to break when required
+	for {
+		s.Fade(a, duration/2)
+		s.Fade(b, duration/2)
+	}
+
+}
+
+func (s *Strip) FadeOut(duration time.Duration) {
+	s.Fade(RGB.Off(), duration)
+}
+
+func (s *Strip) FlashBetween(c []RGB, d time.Duration) {
+
+	// HACK: This will block. Consider using channel to break when required
+	for {
+		for color := range c {
+			s.SetColor(color)
+			time.Sleep(d)
+		}
+	}
+}
+
+func (s *Strip) Flash(c RGB, d time.Duration) {
+	s.FlashBetween(append(c, RGB.Off()), d)
+}
