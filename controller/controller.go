86877a2e91eebcbf1567d755325fed312f4926e9
chaosaffe <jason@chaosaffe.io>
2016-11-21 23:26:11 +0100

    Added fade and effect functions to strip

--- controller/controller.go
+++ controller/controller.go
@@ -107,5 +107,59 @@ func (s *Strip) TestStrip() {
 }
 
 func (s *Strip) Off() {
-	s.SetColor(RGB{Red: 0, Green: 0, Blue: 0})
+	s.SetColor(RGB.Off())
+}
+
+func (s *Strip) FadeBetween(a, b RGB, duration time.Duration) {
+
+	s.Fade(a, duration/2)
+
+	for {
+		s.Fade(a, duration/2)
+		s.Fade(b, duration/2)
+	}
+
+}
+
+func (s *Strip) FadeOut(duration time.Duration) {
+	s.Fade(RGB.Off(), duration)
+}
+
+func (s *Strip) Fade(color RGB, duration time.Duration) {
+
+	// calculate step duration and # of steps
+	stepDuration := time.Duration(100) * time.Millisecond
+	steps := duration / stepDuration
+	maxDiff = s.Color.maxDifference(color)
+
+	if steps > maxDiff {
+		steps = maxDiff
+		stepDuration = duration / steps
+	}
+
+	// calculate differences
+
+	// TODO: Fades should be done in the HSV space for more appeasing gradients
+
+	rDiff, gDiff, bDiff := s.Color.difference(color)
+
+	// calculate change per steps
+
+	// This will cause fractional steps to be discarded, meaning there
+	// is the possibility that changes won't occur, or that the final color
+	// won't be the requested color
+
+	rStep = rDiff / steps
+	gStep = gDiff / steps
+	bStep = bDiff / steps
+
+	for step := 0; step < count; step++ {
+		s.SetColor(s.Color.Add(rStep, gStep, bStep))
+		time.Sleep(stepDuration)
+	}
+
+	// HACK: to ensure the final color is the requested color
+
+	s.SetColor(color)
+
 }
