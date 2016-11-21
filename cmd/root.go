11325ba5416adfd2e652edcb9862b1c4b1979dd2
chaosaffe <jason@chaosaffe.io>
2016-11-21 15:16:31 +0100

    Rough mock of root cmd

--- cmd/root.go
+++ cmd/root.go
@@ -18,6 +18,7 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/chaosaffe/led-strip-controller/controller"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -27,20 +28,18 @@ var cfgFile string
 // RootCmd represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
 	Use:   "led-strip-controller",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Does stuff",
+	Long:  `Does stuff`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) {
+
+		s = controller.NewStrip()
+		defer s.Close()
+
+	},
 }
 
-// Execute adds all child commands to the root command sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	if err := RootCmd.Execute(); err != nil {
 		fmt.Println(err)
