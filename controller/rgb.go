3d1f9b75ed94b71b14fd80a036fa042595122358
chaosaffe <jason@chaosaffe.io>
2016-11-30 00:30:56 +0100

    Changed RGB value type in export comment

--- controller/rgb.go
+++ controller/rgb.go
@@ -1,6 +1,6 @@
 package controller
 
-// RGB is a struct representing the Red, Blue and Green byte values
+// RGB is a struct representing the Red, Blue and Green values
 type RGB struct {
 	Red   float32
 	Green float32
